2893339119af2cf926a7fd638a53d19564e9d9d0
Suguru677 <s.m.yt.939@outlook.jp>
2021-08-26 03:31:27 +0900

    app template

--- dockers/client/server.js
+++ dockers/client/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const serveStatic = require("serve-static")
+var express = require('express');
+var serveStatic = require("serve-static")
 var errorhandler = require('errorhandler');
 const path = require('path');
 app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(express.static(__dirname + '/public'));
-const port = process.env.PORT || 80;
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 8080;
+// app.get("/",function(req,res){
+//     res.sendFile(__dirname+"/index.html");
   
-  });
+//   });
+
 app.listen(port);
